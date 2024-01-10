8ff25573eb128f52844065d6f481ea14944a4ee5
= <=>
2024-01-11 01:57:12 +0530

    build error fix 3

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import DiscordButton from '@/components/Discord'
 import Footer from '@/components/Footer'
 import Skills from '@/components/Skills'
 import Work from '@/components/Work'
-import Projects from '@/components/projects'
-import Image from 'next/image'
 
 export default function Home() {
   return (
